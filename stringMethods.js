13bba3af349ebc1c428520d13e7dc19d23ca4f4d
Harsh Fase <156816848+HarshF26@users.noreply.github.com>
2024-03-23 13:11:57 +0530

    Add files via upload

---
+++ stringMethods.js
@@ -0,0 +1,85 @@
+console.log("string length")
+var str = "Hello, World!";
+console.log(str.length);
+
+console.log("String charAt()")
+var str = "Hello";
+console.log(str.charAt(1)); 
+
+console.log("String charCodeAt()")
+var str = "Hello";
+console.log(str.charCodeAt(1));
+
+console.log("String at()")
+var str = "Hello";
+console.log(str.at(1));
+
+console.log("String [ ]")
+var str = "Hello";
+
+console.log("String slice()")
+console.log(str[1]);
+var str = "Hello, World!";
+
+console.log("String slice")
+console.log(str.slice(7));
+var str = "Hello, World!";
+
+console.log("String substring()")
+console.log(str.substring(7));
+var str = "Hello, World!";
+
+console.log("String substr()")
+console.log(str.substr(7));
+var str = "Hello";
+
+console.log("String toUpperCase()")
+console.log(str.toUpperCase());
+var str = "Hello";
+
+console.log("String toLowerCase()")
+console.log(str.toLowerCase());
+var str1 = "Hello";
+var str2 = "World";
+
+console.log("String concat()")
+console.log(str1.concat(", ", str2)); 
+var str = "  Hello, World!  ";
+
+console.log("String trim()")
+console.log(str.trim()); 
+var str = "  Hello";
+
+console.log("String trimStart()")
+console.log(str.trimStart()); 
+var str = "Hello  ";
+
+console.log("String trimEnd()")
+console.log(str.trimEnd());
+var str = "5";
+
+console.log("String padStart()")
+console.log(str.padStart(3, "0")); 
+var str = "5";
+
+console.log("String padEnd()")
+console.log(str.padEnd(3, "0"));
+var str = "Hello";
+
+console.log("String repeat()")
+console.log(str.repeat(3)); 
+var str = "Hello, World!";
+
+console.log("String replace()")
+console.log(str.replace("World", "JavaScript")); 
+var str = "Hello, World!";
+
+console.log("String replaceAll()")
+console.log(str.replaceAll("o", "X"));
+var str = "Hello, World!";
+
+console.log("String split()")
+console.log(str.split(", "));
+
+
+
